e9f71f2ab86616c698e78b52d0ead644c4f21ece
James Anastasiou <james.anastasiou@gmail.com>
2016-12-01 12:09:23 +1100

    Added dependencies

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Lib
     ( someFunc
     ) where
 
+import Control.Monad.Trans.Reader
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data UrlInfo = 
+    UrlInfo { 
+        _siteurl :: String,
+        _board   :: String,
+        _threadNumber :: String
+}
+
+buildUrl :: Reader UrlInfo String
+buildUrl = do
+    UrlInfo {..} <- ask
+    return $ _siteurl ++ "/" ++ _board ++ "/res/" ++ _threadNumber ++ ".json"
+
